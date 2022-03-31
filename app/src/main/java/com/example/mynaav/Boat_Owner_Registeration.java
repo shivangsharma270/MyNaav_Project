c4c51bdbf3ce843f7c1e923510809bb66a71f58c
Naman Gupta <153namangupta@gmail.com>
2022-03-31 23:42:33 +0530

    commit

---
+++ app/src/main/java/com/example/mynaav/Boat_Owner_Registeration.java
@@ -0,0 +1,30 @@
+package com.example.mynaav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Boat_Owner_Registeration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boat_owner_registeration);
+
+        Spinner myspinner= findViewById(R.id.BoatStand);
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(Boat_Owner_Registeration.this,
+        android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.BoatStand));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(adapter);
+
+        Spinner myspinner2= findViewById(R.id.BoatSize);
+        ArrayAdapter<String> adapter1= new ArrayAdapter<String>(Boat_Owner_Registeration.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.BoatSize));
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner2.setAdapter(adapter1);
+
+    }
+
+}
--- app/src/main/java/com/example/mynaav/Home_Activity.java
+++ app/src/main/java/com/example/mynaav/Home_Activity.java
@@ -2,8 +2,11 @@ package com.example.mynaav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Home_Activity extends AppCompatActivity {
 
@@ -14,6 +17,16 @@ public class Home_Activity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+
+
+        Button demos=(Button) findViewById(R.id.demo);
+        demos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home_Activity.this, Boat_Owner_Registeration.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
