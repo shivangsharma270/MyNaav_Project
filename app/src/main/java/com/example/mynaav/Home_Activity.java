9f141bfa90b080587f3bb748f0200f6ce9187bd9
SHIVANG SHARMA <shivangsharma463@gmail.com>
2022-04-01 21:24:54 +0530

    commit

--- app/src/main/java/com/example/mynaav/Home_Activity.java
+++ app/src/main/java/com/example/mynaav/Home_Activity.java
@@ -1,5 +1,6 @@
 package com.example.mynaav;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,13 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Home_Activity extends AppCompatActivity {
 
@@ -22,15 +30,37 @@ public class Home_Activity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
         phoneno=findViewById(R.id.phoneno);
-        Proceedbtn=findViewById(R.id.Proceedbtn);
-
-
+        Proceedbtn=findViewById(R.id.VERIFY);
         Proceedbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String phonenumber=phoneno.getText().toString();
 
                 boolean check=validateinfo(phonenumber);
+                Proceedbtn.setVisibility(View.INVISIBLE);
+                PhoneAuthProvider.getInstance().verifyPhoneNumber("+91" + phoneno.getText().toString(), 60, TimeUnit.SECONDS,
+                        Home_Activity.this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                Proceedbtn.setVisibility(View.VISIBLE);
+
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Proceedbtn.setVisibility(View.VISIBLE);
+                                Toast.makeText(Home_Activity.this,"Please check your internet connection",Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String backotp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(backotp, forceResendingToken);
+                                Intent intent = new Intent(Home_Activity.this, otpfetch.class);
+                                intent.putExtra("mobile",phoneno.getText().toString());
+                                intent.putExtra("backotp",backotp);
+                                startActivity(intent);
+                            }
+                        });
 
             }
         });
@@ -47,8 +77,11 @@ public class Home_Activity extends AppCompatActivity {
             phoneno.setError("Invalid Number");
             return true;
         } else {
-            Intent intent = new Intent(Home_Activity.this, otpfetch.class);
-            startActivity(intent);
+
+
+
+
+
             return false;
         }
     }
--- app/src/main/java/com/example/mynaav/otpfetch.java
+++ app/src/main/java/com/example/mynaav/otpfetch.java
@@ -1,15 +1,29 @@
 package com.example.mynaav;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class otpfetch extends AppCompatActivity {
-    EditText otp_edit_box1,otp_edit_box2,otp_edit_box3,otp_edit_box4,otp_edit_box5,otp_edit_box6;
+    EditText otp_edit_box1,otp_edit_box2,otp_edit_box3,otp_edit_box4,otp_edit_box5,otp_edit_box6,phonenumber;
+    Button Verifybtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +32,58 @@ public class otpfetch extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_otpfetch);
+        phonenumber=findViewById(R.id.phoneno);
         otp_edit_box1=findViewById(R.id.otp_edit_box1);
         otp_edit_box2=findViewById(R.id.otp_edit_box2);
         otp_edit_box3=findViewById(R.id.otp_edit_box3);
         otp_edit_box4=findViewById(R.id.otp_edit_box4);
         otp_edit_box5=findViewById(R.id.otp_edit_box5);
         otp_edit_box6=findViewById(R.id.otp_edit_box6);
+        String getotpback;
+        //final ProgressBar progressBar=findViewById(R.id.pro)
+         getotpback=getIntent().getStringExtra("backotp");
+        Verifybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                String entercodeotp=otp_edit_box1.getText().toString()+
+                        otp_edit_box2.getText().toString()+
+                        otp_edit_box3.getText().toString()+
+                        otp_edit_box4.getText().toString()+
+                        otp_edit_box5.getText().toString()+
+                        otp_edit_box6.getText().toString();
+
+                if(getotpback!=null){
+                   Verifybtn.setVisibility(View.INVISIBLE);
+                    PhoneAuthCredential phoneAuthCredential=PhoneAuthProvider.getCredential(
+                            getotpback,entercodeotp
+
+                            );
+                    FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent=new Intent(getApplicationContext(),UserData.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    }
+                                    else{
+                                        Toast.makeText(otpfetch.this,"Enter the correct OTP",Toast.LENGTH_SHORT).show();
+
+
+                                    }
+                                }
+                            });
+                }
+                else{
+                    Toast.makeText(otpfetch.this,"Please check your internet connection",Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+        });
         otp_edit_box1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
