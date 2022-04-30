424553dda3f333fcbfd118daade1284b996f40f0
Naman Gupta <153namangupta@gmail.com>
2022-04-30 21:39:15 +0530

    commit

---
+++ app/src/main/java/com/example/mynaav/Boat_Privacy_Policy.java
@@ -0,0 +1,14 @@
+package com.example.mynaav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Boat_Privacy_Policy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boat_privacy_policy);
+    }
+}
